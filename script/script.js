f3b057a1997e2e63a3a1841411c62d1d9f41d7ad
Sylvain Gounon <sylvain.gounon@icloud.com>
2020-10-07 17:43:53 +0200

    on click

--- script/script.js
+++ script/script.js
@@ -1,5 +1,19 @@
 $.noConflict();
 jQuery(document).ready(($) => {
 
+    let count = 0;
+    $('#arrow').on('click', () => {
+        count++;
+        if (count % 2 == 1) {
+            $('#slideUp').css({ height: '62px' });
+            $('#arrow svg path').css({ fill: 'white' });
+            $('#arrow').css({ background: 'hsl(214, 17%, 51%)' });
+        } else {
+            $('#slideUp').css({ height: '0' });
+            $('#arrow svg path').css({ fill: '#6E8098' });
+            $('#arrow').css({ background: 'hsl(210, 46%, 95%)' });
+
+        }
+    })
 
 });
